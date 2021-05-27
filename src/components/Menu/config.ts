0e0fb558bb9307bbd2b0b926479ba8eec6cf478e
Frenchkiss Finance <hello@frenchkiss.finance>
2021-05-27 15:30:27 +0100

    Menu config updated

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,105 +31,36 @@ const config: MenuEntry[] = [
     href: '/pools',
   },
   {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: '/#/',
-    status: {
-      text: 'SOON',
-      color: 'warning',
-    },
-    // href: '/lottery',
-    // status: {
-    //   text: 'WIN',
-    //   color: 'warning',
-    // },
-  },
-  {
     label: 'NFT Marketplace',
-    icon: 'TicketIcon',
-    href: '/#/',
-    status: {
-      text: 'SOON',
-      color: 'warning',
-    },
-    // href: '/lottery',
-    // status: {
-    //   text: 'WIN',
-    //   color: 'warning',
-    // },
+    icon: 'NftIcon',
+    href: 'https://nft.frenchkiss.finance',
   },
-  // {
-  //   label: 'Collectibles',
-  //   icon: 'NftIcon',
-  //   href: '/collectibles',
-  // },
-  // {
-  //   label: 'Team Battle',
-  //   icon: 'TeamBattleIcon',
-  //   href: '/competition',
-  //   status: {
-  //     text: 'CLAIM',
-  //     color: 'warning',
-  //   },
-  // },
-  // {
-  //   label: 'Teams & Profile',
-  //   icon: 'GroupsIcon',
-  //   items: [
-  //     {
-  //       label: 'Leaderboard',
-  //       href: '/teams',
-  //     },
-  //     {
-  //       label: 'Task Center',
-  //       href: '/profile/tasks',
-  //     },
-  //     {
-  //       label: 'Your Profile',
-  //       href: '/profile',
-  //     },
-  //   ],
-  // },
   {
     label: 'Info',
     icon: 'InfoIcon',
-    href: '/#/',
-    status: {
-      text: 'SOON',
-      color: 'warning',
-    },
-    // items: [
-    //   {
-    //     label: 'Overview',
-    //     href: 'https://info.frenchkiss.finance',
-    //   },
-    //   {
-    //     label: 'Tokens',
-    //     href: 'https://info.frenchkiss.finance/tokens',
-    //   },
-    //   {
-    //     label: 'Pairs',
-    //     href: 'https://info.frenchkiss.finance/pairs',
-    //   },
-    //   {
-    //     label: 'Accounts',
-    //     href: 'https://info.frenchkiss.finance/accounts',
-    //   },
-    // ],
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://info.frenchkiss.finance',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://info.frenchkiss.finance/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://info.frenchkiss.finance/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://info.frenchkiss.finance/accounts',
+      },
+    ],
   },
-  // {
-  //   label: 'IFO',
-  //   icon: 'IfoIcon',
-  //   href: '/ifo',
-  // },
   {
     label: 'More',
     icon: 'MoreIcon',
     items: [
-      {
-        label: 'Contact',
-        href: 'https://docs.frenchkiss.finance/contact-us',
-      },
       {
         label: 'Voting',
         // href: 'https://voting.frenchkiss.finance',
@@ -144,17 +75,21 @@ const config: MenuEntry[] = [
         href: 'https://github.com/frenchkiss-finance',
       },
       {
+        label: 'Whitepaper',
+        href: 'https://frenchkiss.finance/doc/whitepaper.pdf',
+      },
+      {
         label: 'Docs',
         href: 'https://docs.frenchkiss.finance',
       },
       {
-        label: 'Blog',
-        href: 'https://frenchkiss-finance.medium.com',
+        label: 'Medium',
+        href: 'https://medium.com/@frenchkiss.finance',
       },
-      // {
-      //   label: 'Merch',
-      //   href: 'https://frenchkiss.creator-spring.com/',
-      // },
+      {
+        label: 'Support',
+        href: 'https://docs.frenchkiss.finance/contact-us',
+      },
     ],
   },
 ]
